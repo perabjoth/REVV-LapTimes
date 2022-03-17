0d030724afb43d1e265abb9ce9a1680d3b3a8c0c
Perabjoth Singh Bajwa <perabjothsinghbajwa@gmail.com>
2022-03-17 12:37:15 +0200

    Add lap times

--- REVVLapTimes.ps1
+++ REVVLapTimes.ps1
@@ -1,14 +1,25 @@
 $transactionID = Read-Host -Prompt 'Input Transaction ID'
 $releases = Invoke-RestMethod -Uri  "https://game-session-api.revvracing.com/v1.0/game/result/$($transactionID)"
 $playerID = $releases.players.playerId
-$results = $releases.data."$playerID"
+$results = $releases.result."$playerID".laps
 $fileData = ""
-$results.PSObject.Properties | ForEach-Object {
-   $name = $_.Name  -split "_"
-   $time = $_.Value/1000
-   $lapTime = "$($name): $($time)"
-   $fileData = $fileData + $lapTime + "`r`n"
+$lapTimes = ""
+$lapCount = 1
+$newline = "`r`n"
+Foreach ($i in $results) {
+   $lapTime = $i.time
+   $lapTime = [timespan]::fromseconds($lapTime / 1000)
+   $lapTime = ("{0:mm\:ss\.fff}" -f $lapTime)
+   $sectorCount = 1
+   $lapTimes = $lapTimes + "Lap " + $lapCount + ": " + $lapTime + $newline
+   Foreach ($j in $i.sectors) {
+      $sectorTime = $j / 1000
+      $fileData = $fileData + "Lap " + $lapCount + " Sector " + $sectorCount + ": " + $sectorTime + $newline
+      $sectorCount = $sectorCount + 1
+   }
+   $lapCount = $lapCount + 1
 }
+$fileData = $lapTimes + $fileData
 $scriptFolder = $MyInvocation.MyCommand.Path | Split-Path -Parent
 $fileOutPut = "$($scriptFolder)\$($transactionID).txt"
 $fileData | Out-File -FilePath $fileOutPut
